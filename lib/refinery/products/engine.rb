72836dc47c3dcae926618d25b5643c7c77b808c8
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2014-07-08 15:52:04 +0800

    initial commit

---
+++ lib/refinery/products/engine.rb
@@ -0,0 +1,27 @@
+module Refinery
+  module Products
+    class Engine < Rails::Engine
+      extend Refinery::Engine
+      isolate_namespace Refinery::Products
+
+      engine_name :refinery_products
+
+      before_inclusion do
+        Refinery::Plugin.register do |plugin|
+          plugin.name = "products"
+          plugin.url = proc { Refinery::Core::Engine.routes.url_helpers.products_admin_products_path }
+          plugin.pathname = root
+          plugin.activity = {
+            :class_name => :'refinery/products/product',
+            :title => 'name'
+          }
+          
+        end
+      end
+
+      config.after_initialize do
+        Refinery.register_extension(Refinery::Products)
+      end
+    end
+  end
+end
